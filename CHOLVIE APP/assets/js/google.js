164fef461988df9006c591a7aad9162ce1b655d6
Christie <cholvie@gmail.com>
2018-09-24 15:11:14 -0400

    first push

---
+++ CHOLVIE APP/assets/js/google.js
@@ -0,0 +1,5 @@
+
+// Google translator app to translate page body
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+  }
